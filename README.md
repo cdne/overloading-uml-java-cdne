fdf810e67ee1bde757f704b894d8f74b54622104
zoltansallay <zoltan.sallay@codecool.com>
2017-08-27 09:43:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java SE: Constructor and method overloading
+
+Java SE module 3rd SI week, UML exercise
---
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -0,0 +1,8 @@
+package com.codecool.uml.overloading;
+
+public class Webshop {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
