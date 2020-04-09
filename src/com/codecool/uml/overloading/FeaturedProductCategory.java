c89137b0d23de56b6bac135a850b05a3c59d0d75
Claudiu <cduneata@gmail.com>
2020-04-09 17:20:57 +0300

    Add toString method

--- src/com/codecool/uml/overloading/FeaturedProductCategory.java
+++ src/com/codecool/uml/overloading/FeaturedProductCategory.java
@@ -1,17 +1,21 @@
 package com.codecool.uml.overloading;
 
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FeaturedProductCategory extends ProductCategory{
-    Date startDate;
-    Date endDate;
+    Date startDate = new Date();
+    Date endDate = new Date();
 
-    void schedule(Date startDate, Date endDate){
+    void schedule(String startDate, String endDate){
+        SimpleDateFormat objSDF = new SimpleDateFormat("dd-mm-yyyy");
 
     }
 
     @Override
     public String toString(){
-        return "";
+        return "FeaturedProductCategory:"+"id: " + getId() + ", name: " + getName() + ", department: " + getDepartment() +
+                ", description: " + getDescription() + ", startDate: " + startDate + ", endDate: " + endDate;
     }
 }
