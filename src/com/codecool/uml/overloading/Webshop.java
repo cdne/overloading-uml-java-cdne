0309ab506692f7ae109687a9d02bf957420c4f70
Claudiu <cduneata@gmail.com>
2020-04-09 13:37:09 +0300

    Add category and products

--- src/com/codecool/uml/overloading/Webshop.java
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -1,8 +1,50 @@
 package com.codecool.uml.overloading;
 
+import java.util.Currency;
+
 public class Webshop {
 
     public static void main(String[] args) {
-	// write your code here
+        //        Adding product categories
+        ProductCategory productCategory1 = new ProductCategory("Computer & Accessories", "Electronics",
+                "Computer accessories and components");
+        ProductCategory dogsCategory = new ProductCategory("Dogs", "Pet Supplies",
+                "Supplies for dogs");
+        ProductCategory productCategory3 = new ProductCategory("Laptop Bags", "Luggage",
+                "Bags for laptop");
+
+        //        Adding suppliers
+        Supplier amazon = new Supplier();
+        amazon.setName("Amazon");
+        amazon.setDescription("Products sold on Amazon");
+        Supplier emag = new Supplier();
+        emag.setName("Emag");
+        emag.setDescription("Products sold on Emag");
+        Supplier ikea = new Supplier();
+        ikea.setName("Ikea");
+        ikea.setDescription("Products sold by Ikea");
+
+//        Adding product 1
+        Product product1 = new Product();
+        product1.setName("Nobby Classic Preno Royal 80640-15 Harness");
+        product1.setDefaultPrice(13);
+        product1.setDefaultCurrency(Currency.getInstance("USD"));
+        product1.setSupplier(amazon);
+        product1.setProductCategory(dogsCategory);
+
+
+//        Adding product 2
+        Product product2 = new Product();
+        product2.setName("21");
+        product2.setDefaultPrice(133);
+        product2.setDefaultCurrency(Currency.getInstance("USD"));
+        product2.setSupplier(amazon);
+        product2.setProductCategory(dogsCategory);
+
+
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProduct(product1);
+        warehouse.addProduct(product2);
+        System.out.println(warehouse.getProducts());
     }
 }
